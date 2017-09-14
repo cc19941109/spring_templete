d4ef067ce092bc1ed12685a8747bd29693d4d71c
cc <15355498770@163.com>
2017-09-15 00:17:17 +0800

    get map/list from obj/bean

--- src/main/java/com/chen/compute/SpringELTest.java
+++ src/main/java/com/chen/compute/SpringELTest.java
@@ -19,8 +19,37 @@ public class SpringELTest {
 		springELTest.testObJ2();
 	}
 
-	
-	public void testObJ2(){
+	public void testList() {
+		UserEntity user = new UserEntity();
+		user.setId(1l);
+		user.setInfo("你好");
+		user.setName("cc");
+
+		ExpressionParser parser = new SpelExpressionParser();
+		EvaluationContext context = new StandardEvaluationContext(user);
+		int[] array1 = (int[]) parser.parseExpression("new int[]{1,2,3}").getValue(context);
+		for (int i : array1) {
+			System.out.println(array1[i - 1]);
+		}
+		// Using an initializer to build a multi-dimensional array is not
+		// currently supported
+		// int[][] array2 = (int[][]) parser.parseExpression("new
+		// int[2][3]{{1,2,3},{4,5,6}}").getValue(context);
+		List list = (List) parser.parseExpression("{1,2,3,4}").getValue();
+		
+		for (Object object : list) {
+			System.out.println("list : " +object);
+		}
+		 context = new StandardEvaluationContext(list);
+		
+		
+		List list2 = (List) parser.parseExpression("{{1,3},{2,4}}").getValue();
+		
+		String string = (String) parser.parseExpression("list[2]").getValue(context);
+		System.out.println(string);
+	}
+
+	public void testObJ2() {
 		Car car = new Car();
 		car.setName("bmw");
 		List<String> names = new ArrayList<>();
@@ -31,27 +60,29 @@ public class SpringELTest {
 		car.setSize(120);
 		Map<String, Integer> owners = new HashMap<>();
 		owners.put("cc1", 1);
-		owners.put("xw1",1);
+		owners.put("xw1", 1);
 		owners.put("zf1", 1);
 		car.setOwners(owners);
-		
+
 		UserEntity user = new UserEntity();
 		user.setId(1l);
 		user.setInfo("你好");
 		user.setName("cc");
 		car.setUserEntity(user);
-		
-		
+
 		ExpressionParser parser = new SpelExpressionParser();
 		EvaluationContext context = new StandardEvaluationContext(car);
 
 		String username = (String) parser.parseExpression("userEntity.name").getValue(context);
 		System.out.println(username);
-		
-		
+		String test1 = (String) parser.parseExpression("names[1]").getValue(context);
+		System.err.println(test1);
+	
+		int test2 = (int) parser.parseExpression("owners['cc1']").getValue(context);
+		System.err.println(test2);
+	
 	}
-	
-	
+
 	public void testObj() {
 
 		UserEntity user = new UserEntity();
@@ -65,7 +96,7 @@ public class SpringELTest {
 		System.out.println(username);
 		long id = (long) parser.parseExpression("id").getValue(context);
 		System.out.println(id);
-		
+
 	}
 
 	public void testBaisc() {
