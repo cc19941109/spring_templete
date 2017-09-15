22a33a6389941a3057184b76e8bca6789eceb223
giraffe tree <15355498770@163.com>
2017-09-15 15:04:40 +0800

    add java8 lambda

---
+++ src/main/java/com/chen/java8/LambdaInterface.java
@@ -0,0 +1,23 @@
+package com.chen.java8;
+
+public class LambdaInterface {
+	@FunctionalInterface
+    interface Predicate<T> {
+        boolean test(T t);
+    }
+	
+	@FunctionalInterface
+    interface Consumer<T> {
+        void accept(T t);
+    }
+	
+	public static void donation(Integer money, Consumer<Integer> consumer){
+	    consumer.accept(money);  
+	}
+	public static void main(String[] args) {
+	    donation(1000, money -> System.out.println("好心的麦乐迪为Blade捐赠了"+money+"元")) ;
+	}
+	
+}
+
+
