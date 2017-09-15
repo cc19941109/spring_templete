a8db03b389c427c82277838eb607640373ab79ea
giraffe tree <15355498770@163.com>
2017-09-15 11:17:37 +0800

    add springEL

--- src/main/java/com/chen/compute/SpringELTest.java
+++ src/main/java/com/chen/compute/SpringELTest.java
@@ -1,6 +1,7 @@
 package com.chen.compute;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,21 +17,57 @@ public class SpringELTest {
 	public static void main(String[] args) {
 		SpringELTest springELTest = new SpringELTest();
 
-		springELTest.testObj();
+		springELTest.testCollectionFilter();
 	}
 
-	public void testElvis(){
-
-		ExpressionParser parser = new SpelExpressionParser();
-	}
-	
-	
-	
-	public void testOperator(){
-		
+	public void testCollectionFilter() {
+		List<Integer> list = Arrays.asList(1,2,30,25,21,3,2231);
 		ExpressionParser parser = new SpelExpressionParser();
 		EvaluationContext context = new StandardEvaluationContext();
+		context.setVariable("list", list);
+		List<Integer> test = (List<Integer>) parser.parseExpression("#list.?[#this>20]").getValue(context);
+		for (Integer integer : test) {
+			System.out.println(integer);
+		}		
+	}
+	
+	public void testVar() {
+		UserEntity userEntity = new UserEntity();
+
+		ExpressionParser parser = new SpelExpressionParser();
+		EvaluationContext context = new StandardEvaluationContext(userEntity);
+		context.setVariable("giveName", "giraffe");
+		String value = parser.parseExpression("#giveName").getValue(context,String.class);
+		System.out.println(value);
 		
+	}
+
+	public void testClass() {
+		ExpressionParser parser = new SpelExpressionParser();
+		// java.lang 包下直接识别s
+		// 可以直接调用类的静态方法
+		Class test = parser.parseExpression("T(String)").getValue(Class.class);
+		System.out.println(test.getName());
+		UserEntity test1 = parser.parseExpression("new com.chen.entity.UserEntity()").getValue(UserEntity.class);
+		System.out.println(test1.getClass().getName());
+
+	}
+
+	public void testElvis() {
+		UserEntity userEntity = new UserEntity();
+
+		ExpressionParser parser = new SpelExpressionParser();
+		EvaluationContext context = new StandardEvaluationContext(userEntity);
+
+		parser.parseExpression("name='giraffe tree'").getValue(context);
+		System.out.println(userEntity.getName());
+	}
+
+	public void testOperator() {
+
+		ExpressionParser parser = new SpelExpressionParser();
+		EvaluationContext context = new StandardEvaluationContext();
+
 		boolean testTrue = parser.parseExpression("2==2").getValue(Boolean.class);
 		System.out.println(testTrue);
 		boolean test1 = parser.parseExpression(" 1 instanceof T(Integer)").getValue(Boolean.class);
@@ -38,9 +75,8 @@ public class SpringELTest {
 		Integer pi = parser.parseExpression(" 2^ 4*2").getValue(Integer.class);
 		System.out.println(pi);
 	}
-	
-	
-	public void testMethod(){
+
+	public void testMethod() {
 		Car car = new Car();
 		car.setName("bmw");
 		List<String> names = new ArrayList<>();
@@ -62,11 +98,11 @@ public class SpringELTest {
 		car.setUserEntity(user);
 
 		ExpressionParser parser = new SpelExpressionParser();
-		EvaluationContext context = new StandardEvaluationContext(car);		
-		boolean isOK = parser.parseExpression("getFalse(1)").getValue(context,Boolean.class);
+		EvaluationContext context = new StandardEvaluationContext(car);
+		boolean isOK = parser.parseExpression("getFalse(1)").getValue(context, Boolean.class);
 		System.out.println(isOK);
 	}
-	
+
 	public void testList() {
 		UserEntity user = new UserEntity();
 		user.setId(1l);
@@ -84,15 +120,14 @@ public class SpringELTest {
 		// int[][] array2 = (int[][]) parser.parseExpression("new
 		// int[2][3]{{1,2,3},{4,5,6}}").getValue(context);
 		List list = (List) parser.parseExpression("{1,2,3,4}").getValue();
-		
+
 		for (Object object : list) {
-			System.out.println("list : " +object);
+			System.out.println("list : " + object);
 		}
-		 context = new StandardEvaluationContext(list);
-		
-		
+		context = new StandardEvaluationContext(list);
+
 		List list2 = (List) parser.parseExpression("{{1,3},{2,4}}").getValue();
-		
+
 		String string = (String) parser.parseExpression("list[2]").getValue(context);
 		System.out.println(string);
 	}
@@ -115,7 +150,7 @@ public class SpringELTest {
 		UserEntity user = new UserEntity();
 		user.setId(1l);
 		user.setInfo("你好");
-//		user.setName("cc");
+		// user.setName("cc");
 		car.setUserEntity(user);
 
 		ExpressionParser parser = new SpelExpressionParser();
@@ -125,10 +160,10 @@ public class SpringELTest {
 		System.out.println(username);
 		String test1 = (String) parser.parseExpression("names[1]").getValue(context);
 		System.err.println(test1);
-	
+
 		int test2 = (int) parser.parseExpression("owners['cc1']").getValue(context);
 		System.err.println(test2);
-	
+
 	}
 
 	public void testObj() {
@@ -136,7 +171,7 @@ public class SpringELTest {
 		UserEntity user = new UserEntity();
 		user.setId(1l);
 		user.setInfo("你好");
-//		user.setName("cc");
+		// user.setName("cc");
 		ExpressionParser parser = new SpelExpressionParser();
 		EvaluationContext context = new StandardEvaluationContext(user);
 
