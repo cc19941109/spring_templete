dc72162459eb85bd55e1b06ce697fa8ac4823fe6
giraffe tree <15355498770@163.com>
2017-09-15 17:49:30 +0800

    add FUNCTION and SUPPLY

--- src/main/java/com/chen/java8/LambdaInterface.java
+++ src/main/java/com/chen/java8/LambdaInterface.java
@@ -11,11 +11,25 @@ public class LambdaInterface {
         void accept(T t);
     }
 	
+	@FunctionalInterface
+    interface Supplier<T> {
+        T get();
+    }
+	
+	@FunctionalInterface
+    interface Function<T, R> {
+        R apply(T t);
+    }
+	
+	
+	
 	public static void donation(Integer money, Consumer<Integer> consumer){
 	    consumer.accept(money);  
 	}
+	
 	public static void main(String[] args) {
 	    donation(1000, money -> System.out.println("好心的麦乐迪为Blade捐赠了"+money+"元")) ;
+	    
 	}
 	
 }
